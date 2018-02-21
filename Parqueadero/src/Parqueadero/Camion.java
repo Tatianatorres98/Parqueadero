63ebb1c2f46ce16142aa0a171d0ebda6ccb4feeb
Estudiantes <Estudiantes@Labing70211>
2018-02-21 16:42:13 -0500

    commit

---
+++ Parqueadero/src/Parqueadero/Camion.java
@@ -0,0 +1,14 @@
+
+package Parqueadero;
+
+
+public class Camion extends Vehiculo {
+    
+    void cargar(){
+        System.out.println("El Camion está cargado");
+    }
+    void descargar(){
+        System.out.println("El Camion está deacargado");
+    }
+    
+}
---
+++ Parqueadero/src/Parqueadero/Vehiculo.java
@@ -0,0 +1,19 @@
+
+package Parqueadero;
+
+
+public class Vehiculo {
+    public String marca;
+    private int cilindraje;
+    private String modelo;
+    private int año;
+    
+    void acelerar(){
+        System.out.println("El vehiculo está acelerando");
+    }
+    void frenar(){
+        System.out.println("El vehiculo está frenando");
+    }
+    
+    
+}
