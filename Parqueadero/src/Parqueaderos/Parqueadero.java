ef9da8eddbddcfff457c0a3e0af3989ea0bcbfa7
Estudiantes <Estudiantes@Labing70211>
2018-02-21 16:50:34 -0500

    commit

---
+++ Parqueadero/src/Parqueaderos/Parqueadero.java
@@ -0,0 +1,23 @@
+
+package Parqueaderos;
+
+import Parqueadero.Camion;
+import Parqueadero.Moto;
+import Parqueadero.Vehiculo;
+
+
+public class Parqueadero {
+    public static void main(String[] args) {
+        
+        Vehiculo vehi;
+        vehi=new Vehiculo();
+        
+        Moto moto1;
+        moto1=new Moto();
+        
+        Camion cami;
+        cami=new Camion();
+        
+       
+    }
+}
