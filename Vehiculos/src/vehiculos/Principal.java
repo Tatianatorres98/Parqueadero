3419a9572934708745578d6966e4cbfeeb016a6e
Estudiantes <Estudiantes@Labing70211>
2018-02-21 16:29:03 -0500

    commit

--- Vehiculos/src/vehiculos/Principal.java
+++ Vehiculos/src/vehiculos/Principal.java
@@ -15,7 +15,17 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Vehiculo vehi;
+        vehi=new Vehiculo();
+        
+        Moto moto1;
+        moto1=new Moto();
+        
+        Camion cami;
+        cami=new Camion();
+        
+       
     }
     
 }
